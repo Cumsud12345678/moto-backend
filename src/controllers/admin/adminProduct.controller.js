e3098b633c923f7d842db1326eb0a1ff024affe4
Cumsud Isayev <isayevcumu@gmail.com>
2026-07-29 15:07:36 +0400

    Admin Products fix lendi

--- src/controllers/admin/adminProduct.controller.js
+++ src/controllers/admin/adminProduct.controller.js
@@ -10,9 +10,9 @@ dayjs.extend(timezone);
 
 const getProducts = async (req, res, next) => {
   try{
-    const page = Number(req.query.page) || 1
     const limit = 2
     const skip = (page - 1) * limit
+    const page = Number(req.query.page) || 1
     const products = await adminProductService.getProducts(skip, limit)
     res.status(200).json({success: true, data: products})
   }catch(err){
