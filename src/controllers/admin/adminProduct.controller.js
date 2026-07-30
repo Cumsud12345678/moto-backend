0a45b808d5c6568cc6a2ae8cdc407fff019e4e1f
Cumsud Isayev <isayevcumu@gmail.com>
2026-07-30 11:30:30 +0400

    Admin Products page fix

--- src/controllers/admin/adminProduct.controller.js
+++ src/controllers/admin/adminProduct.controller.js
@@ -11,8 +11,8 @@ dayjs.extend(timezone);
 const getProducts = async (req, res, next) => {
   try{
     const limit = 2
-    const skip = (page - 1) * limit
     const page = Number(req.query.page) || 1
+    const skip = (page - 1) * limit
     const products = await adminProductService.getProducts(skip, limit)
     res.status(200).json({success: true, data: products})
   }catch(err){
